e7c8197a6383c5f6244a7a243801a2c15db3626d
h.jafarinejad <hosseinjfr72@gmail.com>
2021-12-22 13:44:15 +0330

    Add Expanded Widget

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicess'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: Image.asset('images/dice1.png'),),
+        Expanded(child: Image.asset('images/dice1.png'),),
+      ],
+    );
+  }
+}
+
+
