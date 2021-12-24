9888cbeaa96b14f714a956ad6368ba271605e163
hossein jafarinejad <hosseinjn@outlook.com>
2021-12-24 13:43:56 +0330

    Add Stateful Widget Add Random library Finish the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -23,24 +24,44 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
 
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void ChangeDiceFace(){
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1; // generate number between 0 - 5
+      leftDiceNumber = Random().nextInt(6) + 1; // generate number between 0 - 5
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: TextButton(
+              onPressed: (){
+                ChangeDiceFace();
+              },
+              //String Interpolation
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: TextButton(
+              onPressed: (){
+                // print('right button got pressed');
+                ChangeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
